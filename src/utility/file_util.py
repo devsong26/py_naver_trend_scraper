da0ce682e4933f82459195b116b7aa501e400b60
devsong26 <devsong26@gmail.com>
2020-06-29 08:23:01 +0900

    get root project path

--- src/utility/file_util.py
+++ src/utility/file_util.py
@@ -10,9 +10,11 @@ class FileUtil:
 
     @classmethod
     def get_file_path(cls):
-        # TODO 루트 프로젝트 패쓰를 찾아야 함
         # This Side project used hard coded path
-        project_path = os.path.dirname(os.path.abspath(".gitignore"))
-        # = os.path.dirname(os.path.abspath(__file__))
+        project_path = \
+            os.path.dirname(
+                os.path.dirname(
+                    os.path.dirname(
+                        os.path.abspath(__file__))))
         file_path = os.path.join(project_path, cls.RESOURCES_PATH)
         return file_path
